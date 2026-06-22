93e03fe6383b18aef50857f41f31d9262f68f2de
JOÃO RAFAEL COSTA DO NASCIMENTO <joao.nascimento133@senaisp.edu.br>
2026-06-22 09:50:56 -0300

    deploy

--- backend/src/repositories/produtoRepository.js
+++ backend/src/repositories/produtoRepository.js
@@ -3,16 +3,16 @@ import { connection } from "../configs/Database.js";
 const produtoRepository = {
 
     criar: async (produto) => {
-        const sql = 'INSERT INTO produtos (id_categoria, nome, preco, estoque, imagem) VALUES (?, ?, ?, ?, ?)';
+        const sql = 'INSERT INTO produtos (id_categoria, nome, preco, estoque, imagem) VALUES (?,?,?,?,?)';
         
-        // Extraindo os valores garantindo o uso correto dos getters públicos da classe Produto
-        const idCategoria = produto.idCategoria;
-        const nome = produto.nomeProduto;
-        const preco = produto.valor;
-        const estoque = produto.estoque;
-        const imagem = produto.vinculoImagem;
-
-        const values = [idCategoria, nome, preco, estoque, imagem];
+       
+        const values = [
+            Number(produto.idCategoria), 
+            String(produto.nomeProduto), 
+            Number(produto.valor), 
+            Number(produto.estoque), 
+            String(produto.vinculoImagem)
+        ];
         
         const [rows] = await connection.execute(sql, values);
         return rows;
@@ -22,10 +22,10 @@ const produtoRepository = {
         const sql = 'UPDATE produtos SET preco = ?, estoque = ?, imagem = ? WHERE id = ?';
         
         const values = [
-            produto.valor, 
-            produto.estoque, 
-            produto.vinculoImagem, 
-            produto.id
+            Number(produto.valor), 
+            Number(produto.estoque), 
+            String(produto.vinculoImagem), 
+            Number(produto.id)
         ];
         
         const [rows] = await connection.execute(sql, values);
